7e721762a20a918971cde46b133f8ff75b1d9ee6
Lindet <oksana_kiw@mail.ru>
2014-10-10 00:47:56 +0400

    Nearly ready
    Still need to create function for moving layers

---
+++ Paint/LayerPanel.xaml.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Packaging;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Paint
+{
+    /// <summary>
+    /// Interaction logic for LayerPanel.xaml
+    /// </summary>
+    public partial class LayerPanel : UserControl
+    {
+
+        #region Events
+        public event LayerNameChanged NameChanged;
+        public delegate void LayerNameChanged(object sender, NameArgs args);
+
+        public event ActivePropertyChanged ActiveChanged;
+        public delegate void ActivePropertyChanged(object sender, EventArgs e);
+
+        public event VisibilityPropertyChanged VisibilityChanged;
+        public delegate void VisibilityPropertyChanged(object sender, EventArgs e);
+
+        public event DeleteThisLayer DeleteLayerEvent;
+        public delegate void DeleteThisLayer(object sender, EventArgs e);
+        #endregion
+        public bool Active = false;
+        private bool Visible = true;
+        public string Name;
+        
+        public LayerPanel(string name)
+        {
+            InitializeComponent();
+            NameTextBlock.Text = name;
+            Name = name;
+        }
+
+        //public LayerPanel(string name, bool bydefault)
+        //{
+        //    InitializeComponent();
+        //    NameTextBlock.Text = name;
+        //    Name = name;
+        //    Active = true;
+        //    PanelBorder.Background = Brushes.CadetBlue;
+        //}
+
+        private void EditLayerName(object sender, RoutedEventArgs e)
+        {
+            NameTextBlock.Visibility = Visibility.Collapsed;
+            EditNameTextBox.Visibility = Visibility.Visible;
+            EditNameTextBox.Focus();
+        }
+
+        private void EditName_LostFocus(object sender, RoutedEventArgs e)
+        {
+            var prevName = NameTextBlock.Text;
+            NameTextBlock.Text = EditNameTextBox.Text;
+            Name = EditNameTextBox.Text;
+            EditNameTextBox.Visibility = Visibility.Collapsed;
+            NameTextBlock.Visibility = Visibility.Visible;
+            NameChanged(this, new NameArgs(prevName, Name));
+        }
+
+        private void EditName_EnterPressed(object sender, KeyEventArgs e)
+        {
+            if (e.Key != Key.Enter) return;
+            var prevName = NameTextBlock.Text;
+            NameTextBlock.Text = EditNameTextBox.Text;
+            Name = EditNameTextBox.Text;
+            EditNameTextBox.Visibility = Visibility.Collapsed;
+            NameTextBlock.Visibility = Visibility.Visible;
+
+            NameChanged(this, new NameArgs(prevName, Name));
+        }
+
+        private void SetPanelActive(object sender, MouseButtonEventArgs e)
+        {
+            if (Active) return;
+            if (e.ClickCount != 2) return;
+            Active = true;
+            ActiveChanged(this, new EventArgs());
+            PanelBorder.Background = Brushes.CadetBlue;
+        }
+
+        public void DeactivatePanel()
+        {
+            Active = false;
+            PanelBorder.Background = Brushes.White;
+        }
+
+        public void ActivatePanel()
+        {
+            Active = false;
+            PanelBorder.Background = Brushes.CadetBlue;
+            ActiveChanged(this, new EventArgs());
+        }
+
+        private void VisibilityButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (Visible)
+            {
+                VisibilityImage.Source = new BitmapImage(new Uri("pack://application:,,,/Icons/blocked.png"));
+                Visible = false;
+            }
+            else
+            {
+                VisibilityImage.Source = new BitmapImage(new Uri("pack://application:,,,/Icons/Eye_blue.png"));
+                Visible = true;
+            }
+            VisibilityChanged(this, new EventArgs());
+        }
+
+        private void DeleteLayerButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            DeleteLayerEvent(this, new EventArgs());
+        }
+    }
+
+    public class NameArgs : EventArgs
+    {
+        private string _prevName;
+        private string _newName;
+
+        public string PrevName
+        {
+            get { return _prevName; }
+        }
+
+        public string NewName
+        {
+            get { return _newName; }
+        }
+
+        public NameArgs(string previous, string current)
+        {
+            _prevName = previous;
+            _newName = current;
+        }
+    }
+
+
+    
+}
--- Paint/MainWindow.xaml.cs
+++ Paint/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -7,10 +9,13 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Ink;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -25,14 +30,85 @@ namespace Paint
     {
         private Point PrevPosition;
         private bool HasPreviosPoint = false;
-        private Canvas currentControl; 
+        private Brush currentColor = Brushes.Black;
+        private readonly ObservableCollection<string> layersList = new ObservableCollection<string>();
+        private Canvas currentCanvas;
+        private LayerPanel currentPanel;
+        private int currentNumber = 0;
+       
+        
         public MainWindow()
         {
+            DataContext = this;
             InitializeComponent();
-            currentControl = FirstLayer;
+            CreateNewLayer("DefaultLayer");
         }
 
-        private void Canvas_MouseDown(object sender, MouseEventArgs e)
+        private void CreateNewLayer()
+        {
+            var newLayer = new Canvas() { Style = (Style)FindResource("PaintCanvaStyle"), Name = "NewLayer" + currentNumber, Width = 1500, Height = 1500};
+            currentNumber++;
+            LayoutGrid.Children.Add(newLayer);
+            currentCanvas = newLayer;
+            layersList.Add(newLayer.Name);
+            var temp = new LayerPanel(newLayer.Name);
+            temp.VisibilityChanged += VisibilityChanged;
+            temp.DeleteLayerEvent += DeleteLayer;
+            temp.NameChanged += LayerNameChanged;
+            temp.ActiveChanged += ActiveChanged;
+            LayerStackPanel.Children.Add(temp);
+
+            LayerListExpander.IsExpanded = true;
+            temp.ActivatePanel();
+        }
+
+        private void CreateNewLayer(string LayerName)
+        {
+            var newLayer = new Canvas() { Style = (Style)FindResource("PaintCanvaStyle"), Width = 1500, Height = 1500};
+            LayoutGrid.Children.Add(newLayer);
+            currentCanvas = newLayer;
+            layersList.Add(newLayer.Name);
+            var temp = new LayerPanel(newLayer.Name);
+            temp.VisibilityChanged += VisibilityChanged;
+            temp.DeleteLayerEvent += DeleteLayer;
+            temp.ActiveChanged += ActiveChanged;
+            temp.NameChanged += LayerNameChanged;
+            LayerStackPanel.Children.Add(temp);
+
+            temp.ActivatePanel();
+            LayerListExpander.IsExpanded = true;
+        }
+
+        
+
+        #region Events
+
+        private void ActiveChanged(object sender, EventArgs eventArgs)
+        {
+            var item = sender as LayerPanel;
+            if (currentPanel != null) currentPanel.DeactivatePanel();
+            currentPanel = item;
+            currentCanvas = LayoutGrid.Children.OfType<Canvas>().FirstOrDefault(z => z.Name == currentPanel.Name);
+        }
+
+        private void DeleteLayer(object sender, EventArgs e)
+        {
+            var item = sender as LayerPanel;
+            LayoutGrid.Children.Remove(LayoutGrid.Children.OfType<Canvas>().FirstOrDefault(z => z.Name == item.Name));
+            LayerStackPanel.Children.Remove(item);
+        }
+
+        private void VisibilityChanged(object sender, EventArgs e)
+        {
+            var item = sender as LayerPanel;
+            LayoutGrid.Children.OfType<Canvas>().FirstOrDefault(z => z.Name == item.Name).Visibility =
+                LayoutGrid.Children.OfType<Canvas>().FirstOrDefault(z => z.Name == item.Name).Visibility ==
+                Visibility.Visible
+                    ? Visibility.Collapsed
+                    : Visibility.Visible;
+        }
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
         {
             if (e.LeftButton != MouseButtonState.Pressed) return;
             if (!HasPreviosPoint)
@@ -42,26 +118,22 @@ namespace Paint
             }
             else
             {
-                Brush brush = Brushes.Transparent;
-                switch (currentControl.Name)
-                {
-                    case "FirstLayer":
-                        brush = Brushes.Red;
-                        break;
-                    case "SecondLayer":
-                        brush = Brushes.Green;
-                        break;
-                    case "ThirdLayer":
-                        brush = Brushes.Blue;
-                        break;
-
-                }
+                if (currentCanvas.Visibility != Visibility.Visible) return;
                 var point = e.GetPosition(sender as Canvas);
-                var line = new Line() {Stroke = brush, X1 = PrevPosition.X, Y1 = PrevPosition.Y, X2 = point.X, Y2 = point.Y, StrokeThickness = 2};
-                currentControl.Children.Add(line);
-                UpdateLayout();
+                var line = new Line() { Stroke = currentColor, X1 = PrevPosition.X, Y1 = PrevPosition.Y, X2 = point.X, Y2 = point.Y, StrokeThickness = 2};
+                currentCanvas.Children.Add(line);
                 PrevPosition = point;
             }
+        }
+
+        private void LayerNameChanged(object sender, NameArgs e)
+        {
+            for (var i = 0; i < layersList.Count; i++)
+            {
+                if (layersList[i] == e.PrevName) layersList[i] = e.NewName;
+            }
+
+            LayoutGrid.Children.OfType<Canvas>().FirstOrDefault(z => z.Name == e.PrevName).Name = e.NewName;
         }
 
         private void Canvas_MouseUp(object sender, MouseButtonEventArgs e)
@@ -75,23 +147,20 @@ namespace Paint
             var temp = sender as Button;
             switch (temp.Name)
             {
-                case "FirstButton":
-                    currentControl = FirstLayer;
+                case "NewLayerButton":
+                    CreateNewLayer();
                     break;
-                case "SecondButton":
-                    currentControl = SecondLayer;
-                    break;
-                case "ThirdButton":
-                    currentControl = ThirdLayer;
-                    break;
-                  
             }
-            currentControlTextBlock.Text = "Current Control : " + currentControl.Name;
+        }
+
+        private void ColorButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            currentColor = (sender as Button).Background;
         }
 
         private void ClearAllCanvas(object sender, RoutedEventArgs e)
         {
-            foreach (var item in grid.Children)
+            foreach (var item in LayoutGrid.Children)
             {
                 if(item is Canvas)
                     (item as Canvas).Children.Clear();
@@ -101,6 +170,61 @@ namespace Paint
         private void LeavePaintArea(object sender, MouseEventArgs e)
         {
             HasPreviosPoint = false;
+        }
+
+        //private void CurrentLayerChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    var selectedName = (sender as ListBox).SelectedItem.ToString();
+
+        //    foreach (var item in Grid.Children)
+        //    {
+        //        if (!(item is Canvas)) continue;
+        //        if ((item as Canvas).Name == selectedName)
+        //            currentControl = item as Canvas;
+        //    }
+
+        //}
+
+        #endregion
+
+
+        #region MenuEvents
+            private void AppMenu_ExitButtonClick(object sender, MouseButtonEventArgs e)
+            {
+                App.Current.Shutdown();
+            }
+
+        private void AppMenu_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton != MouseButton.Left) return;
+
+
+            if (e.ClickCount == 2)
+                //Двойное нажатие на верхнюю панель максимизирует окно или возвращает его к исходным размерам.
+            {
+                Window window = Window.GetWindow((Border) sender);
+                window.WindowState = window.WindowState == WindowState.Maximized
+                    ? WindowState.Normal
+                    : WindowState.Maximized;
+
+                LayerListExpander.MaxHeight = SecondRowDefinition.ActualHeight-160;
+
+            }
+            else 
+                //А если нажатие было одно, то позволяет перетаскивать окно.
+            {
+                this.DragMove();
+            }
+
+
+        }
+
+        #endregion
+
+        private void Default_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            HasPreviosPoint = true;
+            PrevPosition = e.GetPosition(sender as Image);
         }
     }
 }
